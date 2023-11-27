c1cd6d25388ef0f963eac4928ff91848db241417
cunha-joao <cunha.joao@ipvc.pt>
2023-11-27 11:26:13 +0000

    added enum classes

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,16 +1,15 @@
 import java.util.*;
+
 public class Cliente extends Utilizador {
     private List<Marcacao> marcacoes;
 
-    // Construtor
-    public Cliente(String nome, String numeroCartaoCidadao, String numeroFiscal, String telefone, String morada, String localidade) {
+    public Cliente(String nome, String numeroCartaoCidadao, String numeroFiscal, String telefone,
+                   String morada, String localidade) {
         super(nome, numeroCartaoCidadao, numeroFiscal, telefone, morada, localidade);
         this.marcacoes = new ArrayList<>();
     }
 
-    public List<Marcacao> consultarMarcações() {
+    public List<Marcacao> getMarcacoes() {
         return marcacoes;
     }
-
-    // Outros métodos relacionados ao Cliente
 }
