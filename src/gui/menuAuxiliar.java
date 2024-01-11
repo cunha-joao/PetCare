d9b25b6e7fb97103e20a9621d61ce3f8003c86f5
cunha-joao <cunha.joao@ipvc.pt>
2024-01-11 19:12:31 +0000

    added update user info

--- src/gui/menuAuxiliar.java
+++ src/gui/menuAuxiliar.java
@@ -3,13 +3,22 @@ package gui;
 import bll.Utilizador;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menuAuxiliar extends JFrame{
     private JPanel menuAuxiliar;
     private JButton consultarMarcacoesButton;
     private JButton sairButton;
 
-    public menuAuxiliar(Utilizador utilizador){}
+    public menuAuxiliar(Utilizador utilizador){
+        sairButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuAuxiliar.this.dispose();
+            }
+        });
+    }
 
     public JPanel getPanel() {
         return menuAuxiliar;
--- src/gui/menuEducador.java
+++ src/gui/menuEducador.java
@@ -3,12 +3,21 @@ package gui;
 import bll.Utilizador;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menuEducador extends JFrame{
     private JPanel menuEdu;
     private JButton sairButton;
 
-    public menuEducador(Utilizador utilizador){}
+    public menuEducador(Utilizador utilizador){
+        sairButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuEducador.this.dispose();
+            }
+        });
+    }
 
     public JPanel getPanel() {
         return menuEdu;
